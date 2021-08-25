bbbbc861a91bf4dfbf9f14b85915d7a0f6fbc1d5
y-noda <y-noda@entaworks.co.jp>
2021-08-25 13:47:12 +0900

    [add]ユーザ登録機能

---
+++ api/app/Http/Controllers/AuthController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+
+    public function register(RegisterRequest $request)
+    {
+        $user = User::create($request->only('first_name', 'last_name', 'email')
+            + [
+                'password' => \Hash::make($request->input('password')),
+                'is_admin' => 1
+            ]
+        );
+        return response($user, Response::HTTP_CREATED);
+    }
+}
+
+
+
+
