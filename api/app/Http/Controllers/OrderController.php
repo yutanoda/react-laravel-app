f9b3638a370499b776fa2b3ff563eedd6c17975e
y-noda <y-noda@entaworks.co.jp>
2021-08-30 00:52:28 +0900

    [add]store_orders

--- api/app/Http/Controllers/OrderController.php
+++ api/app/Http/Controllers/OrderController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\OrderResource;
+use App\Models\Link;
+use App\Models\Product;
 use App\Models\Order;
+use App\Models\OrderItem;
 use Illuminate\Http\Request;
 
 
@@ -12,5 +15,43 @@ class OrderController extends Controller
     public function index()
     {
         return OrderResource::collection(Order::with('orderItems')->get());
+    }
+
+    public function store(Request $request)
+    {
+        if (!$link = Link::where('code', $request->input('code'))->first()) {
+            abort(400, 'Invalid code');
+        }
+
+        $order = new Order();
+
+        $order->code = $link->code;
+        $order->user_id = $link->user->id;
+        $order->ambassador_email = $link->user->email;
+        $order->first_name = $request->input('first_name');
+        $order->last_name = $request->input('last_name');
+        $order->email = $request->input('email');
+        $order->address = $request->input('address');
+        $order->country = $request->input('country');
+        $order->city = $request->input('city');
+        $order->zip = $request->input('zip');
+
+        $order->save();
+
+        foreach ($request->input('products') as $item) {
+            $product = Product::find($item['product_id']);
+
+            $orderItem = new OrderItem();
+            $orderItem->order_id = $order->id;
+            $orderItem->product_title = $product->title;
+            $orderItem->price = $product->price;
+            $orderItem->quantity = $item['quantity'];
+            $orderItem->ambassador_revenue = 0.1 * $product->price * $item['quantity'];
+            $orderItem->admin_revenue = 0.9 * $product->price * $item['quantity'];
+
+            $orderItem->save();
+        }
+
+        return $order->load('orderItems');
     }
 }
--- api/routes/api.php
+++ api/routes/api.php
@@ -63,5 +63,6 @@ Route::prefix('ambassador')->group(function () {
 //Checkout
 Route::prefix('checkout')->group(function () {
     Route::get('links/{code}', [LinkController::class, 'show']);
+    Route::post('orders', [OrderController::class, 'store']);
 });
 
